5ab14cfc5f8d5acfc8d594291c20679923a615f4
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-03-08 21:26:02 +0100

    search v2

--- Solceller/src/main/java/com/example/solceller/HourByHour.java
+++ Solceller/src/main/java/com/example/solceller/HourByHour.java
@@ -3,6 +3,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 public class HourByHour implements iChangeScene
@@ -12,14 +15,17 @@ public class HourByHour implements iChangeScene
 
     public Scene createHourByHourScene()
     {
+        ChoiceBox choiceBox = new ChoiceBox();
 
+        choiceBox.setLayoutX(300);
         buttonBack.setPrefWidth(50);
         // create the content for the hour by hour view
         AnchorPane anchorPane = new AnchorPane();
         date.getDatePicker(anchorPane);
+        date.searchSite(anchorPane,choiceBox);
         buttonBack.setPrefWidth(50);
 
-        anchorPane.getChildren().add(buttonBack);
+        anchorPane.getChildren().addAll(buttonBack,choiceBox);
 
         Scene scene = new Scene(anchorPane, 950, 700);
 
--- Solceller/src/main/java/com/example/solceller/findDate.java
+++ Solceller/src/main/java/com/example/solceller/findDate.java
@@ -1,7 +1,10 @@
 package com.example.solceller;
 
 import javafx.event.EventHandler;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
@@ -22,8 +25,7 @@ public class findDate
             @Override
             public void handle(KeyEvent keyEvent)
             {
-                if (keyEvent.getCode().equals(KeyCode.ENTER))
-                {
+                if (keyEvent.getCode().equals(KeyCode.ENTER)) {
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                     String formattedValue = datePicker.getValue().format(formatter);
 
@@ -32,5 +34,43 @@ public class findDate
             }
         });
         return datePicker;
+    }
+    String siteID = "";
+    public ChoiceBox searchSite(AnchorPane anchorPane, ChoiceBox choiceBox)
+    {
+        for (int i = 0; i < HelloController.sites.size(); i++) {
+            siteID = String.valueOf(HelloController.sites.get(i).getSiteID());
+            choiceBox.getItems().add(siteID);
+        }
+        TextField SearchField = new TextField();
+        SearchField.setLayoutX(20);
+        SearchField.setLayoutY(500);
+
+        SearchField.setOnKeyPressed(new EventHandler<KeyEvent>()
+        {
+            @Override
+            public void handle(KeyEvent keyEvent)
+            {
+
+                if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                    for (int i = 0; i < HelloController.sites.size(); i++) {
+                       // System.out.println(HelloController.sites.get(i).getSiteID());
+                        siteID = String.valueOf(HelloController.sites.get(i).getSiteID());
+
+                        if ((siteID.contains(SearchField.getText()))) {
+                            choiceBox.getItems().remove(siteID);
+                            choiceBox.getItems().add(siteID);
+                        } else {
+                            choiceBox.getItems().remove(siteID);
+                        }
+                    }
+
+                }
+            }
+        });
+        anchorPane.getChildren().add(SearchField);
+
+        return choiceBox;
+
     }
 }
