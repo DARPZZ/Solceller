295a0339acdbcd3ee2e345862345f9a7a79e7159
Astralstr1ke <99681023+Astralstr1ke@users.noreply.github.com>
2023-03-09 09:19:35 +0100

    File helper, recursion fixed hopefully

--- Solceller/src/main/java/com/example/solceller/HelloApplication.java
+++ Solceller/src/main/java/com/example/solceller/HelloApplication.java
@@ -23,6 +23,7 @@ public class HelloApplication extends Application
         stage.setScene(scene);
         stage.show();
         FileHelper.initializeFromFile();
+        System.out.println("");
     }
 
 
